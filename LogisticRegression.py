c5299d13e65d5ad390b4ff23d3918dfe804c2165
Richard <laazydeveloper@gmail.com>
2022-12-10 23:47:38 +0000

    Create LogisticRegression.py
    Logistic Regression code with accuracy of 0.782

---
+++ LogisticRegression.py
@@ -0,0 +1,54 @@
+#%% Loading Data
+
+import pandas as pd
+
+filename = "top10features.csv"
+
+df = pd.read_csv(filename)
+df.info()
+
+
+#%% Remove first column of df
+
+df.drop(df.columns[0], axis=1, inplace=True)
+df
+
+
+#%% Add the Y value to the dataframe
+predictor = pd.read_csv('cleaned.csv')
+temp = pd.DataFrame(predictor.iloc[: , [1]])
+
+# Append the Y value
+df['outcome'] = temp
+df
+
+#%% Outcomes
+effective = df[df.outcome == 1]
+not_effective = df[df.outcome == 0]
+
+x = df.iloc[:, 0:10]
+y = df.iloc[:, [-1]]
+
+#%% Preparing the model
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.25, random_state = 8675309)
+
+# Load Logistic Regression model from SkLearn
+from sklearn.linear_model import LogisticRegression
+model = LogisticRegression()
+
+model.fit(X_train, y_train)
+model.score(X_test,y_test)
+
+
+#%%
+# print(model.predict(x))
+print(y)
+
+#%% Plotting the graphs
+import matplotlib.pyplot as plt
+
+plt.scatter(x, y,color='g')
+plt.plot(x, model.predict(x), color='k')
+
+plt.show()
